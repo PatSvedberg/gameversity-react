f6b515fd382df52b37ff68e942dafe2977757215
PatSvedberg <pat.svedberg@gmail.com>
2023-06-08 09:12:43 +0000

    Add navbar and temporary logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
